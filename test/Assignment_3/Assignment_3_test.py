2f45190858af6dd5654214999c07d365034ccc84
VinayBoddu <vinay.boddu@Diggibyte.com>
2023-07-31 19:15:48 +0530

    File containing the test file related to assignment 3

---
+++ test/Assignment_3/Assignment_3_test.py
@@ -0,0 +1,15 @@
+import unittest
+from src.Assignment_3.Util import *
+from src.Assignment_3.Driver import *
+
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        input=
+        output="My name is vinay and my email is vinay@gmail.com and my gender is Male"
+        self.assertEqual(input, output)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()

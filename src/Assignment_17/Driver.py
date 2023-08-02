3e5e02ee067dc8168bd17fa4716fc2a6f6563f9f
VinayBoddu <vinay.boddu@Diggibyte.com>
2023-08-02 19:31:03 +0530

    Driver file for assignment_17

---
+++ src/Assignment_17/Driver.py
@@ -0,0 +1,11 @@
+from src.Assignment_17.Util import *
+
+# n = int(input())
+# emails = [input().strip() for _ in range(n)]
+#
+# valid_emails = list(filter(validemail, emails))
+# valid_emails.sort()
+#
+# print(valid_emails)
+
+validateemail()
